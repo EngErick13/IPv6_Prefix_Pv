b6d7f086bd4d509c02685f72453df6975bc181cc
EngErick13 <117367058+EngErick13@users.noreply.github.com>
2022-12-10 12:13:53 +0000

    Finalizado a adicao de funcoes

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ def imp_list (_nomefile_):#Função de coleta dos dados de arquivos e salvar em 
         d.insert(c,b[c].replace("\n", ""))
         c=c+1
     return d
-
 def finaliprede (_IP_): #Retorna o final do IP[String] / Retorna a rede do IP[String]
     a,b,c = 0,0,0
     finalip = ["","",""]
@@ -35,11 +34,10 @@ def finaliprede (_IP_): #Retorna o final do IP[String] / Retorna a rede do IP[St
         finalip[-3] = '0'
                         #Fim Correção
                         #Inicia o processo de remoção do "." da mascara   
-    a = len(redeip)                                     #Contabiliza a quantidade de caractere da rede
+    a = len(redeip)-1                                    #Contabiliza a quantidade de caractere da rede
     redeip = list(redeip)                               #converte para tipo lista
     redeip = redeip[0:a-1]                              #remove o ultimo "."
     return [''.join(finalip), ''.join(redeip)]
-
 def tipodevice (_devicename_):
     a,b,c = 0,0,0
     tipo = ["","","","","","","","","","","",""]
@@ -50,7 +48,20 @@ def tipodevice (_devicename_):
         elif _devicename_[c] == "-":
             a += 1
         c+=1
-    return ''.join(tipo)
+    a,b,tipo = len(DeviceTypeTag),0,''.join(tipo)
+    while b <= a-1:
+        if tipo == DeviceTypeTag[b]:
+            tipo = DeviceTypeTagID[b]
+            break
+        b += 1
+    return tipo
+def tagloopback (_rede_):
+    a,b = len(Loopback_tag),0
+    while b <= a-1:
+        if _rede_ == Loopback_tag[b]:
+            break
+        b += 1
+    return loopback_tag_id[b]
 
 DeviceTypeTag = imp_list("devicetype.txt")
 DeviceTypeTagID = imp_list("devicetypeid.txt")
@@ -67,69 +78,12 @@ Hostname_A = "BR-PA-SRM-SEP-PE-01"
 IP_B = "100.65.255.5"
 Hostname_B = "BR-PA-SRM-SAM-PE-01"
 
-#Por enquanto tipo backbone apenas
-T_p2p = "0000:0000:"
-
-
-tagLoopIpB = 0
-tagLoopIpA = 0
-cont_TagL = 0
-
+T_p2p = "0000:0000:"                        #Por enquanto tipo backbone apenas
 fimipa,redeIpA = finaliprede(IP_A)
-
 fimipb,redeIpB = finaliprede(IP_B)
-
 TipoDeviceA = tipodevice(Hostname_A)
-
 TipoDeviceB = tipodevice(Hostname_B)
-
-
-
-
-#Avaliando o total de TAGs Loopback
-total_tags = int(len(Loopback_tag))
-
-#Analise e comparação das tags loopback lado A com o valor inserido
-while cont_TagL <= total_tags-1:
-    if redeIpA == Loopback_tag[cont_TagL]:
-        tagLoopIpA = cont_TagL
-        break
-    cont_TagL += 1
-
-#Zerando a variável para o próximo While
-cont_TagL = 0
-#Analise e comparação das tags loopback lado B com o valor inserido
-while cont_TagL <= total_tags-1:
-    if redeIpB == Loopback_tag[cont_TagL]:
-        tagLoopIpB = cont_TagL
-        break
-    cont_TagL += 1
-
-#Avaliando o total de TAGs Devices
-total_tags = int(len(DeviceTypeTag))
-
-#Zerando a variável para o próximo While
-cont_TagL = 0
-#Analise e comparação das tags devices lado A com o valor inserido
-while cont_TagL <= total_tags-1:
-    if TipoDeviceA == DeviceTypeTag[cont_TagL]:
-        TipoDeviceA = DeviceTypeTagID[cont_TagL]
-        break
-    cont_TagL += 1
-
-#Zerando a variável para o próximo While
-cont_TagL = 0
-#Analise e comparação das tags devices lado B com o valor inserido
-while cont_TagL <= total_tags-1:
-    if TipoDeviceB == DeviceTypeTag[cont_TagL]:
-        TipoDeviceB = DeviceTypeTagID[cont_TagL]
-        print(TipoDeviceB)
-        break
-    cont_TagL += 1
-
+tagLoopIpA = tagloopback(redeIpA)
+tagLoopIpB = tagloopback(redeIpB)
 #Imprimindo o prefixo final (Ex: FD00:0:0:2111:2117:0:0:0/126)
-#.join(fim_IpA) -> Realiza a conversão de lista para string
-#print("\nPrefixo Gerado é:\n""fd00:" + T_p2p+ TipoDeviceA + ''.join(fim_IpA) + ":" + TipoDeviceB + ''.join(fim_IpB) + ":" + loopback_tag_id[tagLoopIpA] + ":" + loopback_tag_id[tagLoopIpB] + ":" + "0000/126" + "\n")
-print("\nPrefixo Gerado é:\n""fd00:" + T_p2p+ TipoDeviceA + fimipa + ":" + TipoDeviceB + fimipb + ":" + loopback_tag_id[tagLoopIpA] + ":" + loopback_tag_id[tagLoopIpB] + ":" + "0000/126" + "\n")
-
-print("fim")
+print("\nPrefixo Gerado é:\n""fd00:" + T_p2p+ TipoDeviceA + fimipa + ":" + TipoDeviceB + fimipb + ":" + tagLoopIpA + ":" + tagLoopIpB + ":" + "0000/126" + "\n")
